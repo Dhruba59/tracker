1d47359e6c1da4cd8657c749eac2ebec6b78e174
Dhruba Jyoti Sarker <dhruba59.sust@gmail.com>
2023-08-23 16:34:47 +0600

    fix sidebar list update & add header dropdown

--- src/components/layouts/base-layout/base-header/base-header.tsx
+++ src/components/layouts/base-layout/base-header/base-header.tsx
@@ -1,11 +1,39 @@
-import { Avatar, Badge, Layout, Menu, MenuProps } from 'antd';
+import { Avatar, Badge, Layout, Menu, MenuProps, Popover, Typography } from 'antd';
 
-import { LogoIcon, NotificationIcon, QuestionCircle, SettingsIcon, WorkspaceIcon } from '@icons';
+import { LogOutIcon, LogoIcon, NotificationIcon, QuestionCircle, SettingsIcon, WorkspaceIcon } from '@icons';
 import './base-header.css';
+import { useState } from 'react';
+import { userLogout } from '@services/auth-services';
 
 const { Header } = Layout;
+const { Text } = Typography;
 
 const BaseHeader = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsPopupOpen(!isPopupOpen);
+  };
+  const handleLogout = () => {
+    userLogout();
+  };
+
+  const content = (
+    <div className='header-popover-content-container'>
+      <div className='header-popover-item'>
+        <span className='header-popover-item-icon' >
+          <Avatar size='small'/>
+        </span>
+        <Text>John doe</Text>
+      </div>
+      <div className="header-popover-item" onClick={handleLogout}>
+        <span className='header-popover-item-icon' >
+          <LogOutIcon />
+        </span>
+        <Text>Logout</Text>
+      </div>
+    </div>
+  );
 
   const items: MenuProps['items'] = [
     {
@@ -25,15 +53,24 @@ const BaseHeader = () => {
     {
       key: 'avatar',
       icon: <Avatar />,
-      label: 'John doe',
-      className: 'header-menu-item'
+      label: (
+        <div>
+          <span>John Doe</span>
+          <Popover 
+            rootClassName='header-popover' 
+            open={isPopupOpen} 
+            content={content} 
+            placement='bottomLeft'/>
+        </div> ),
+      className: 'header-menu-item',
+      onClick: handleClick
     },
   ];
 
   return (
     <Header className='header'>
       <LogoIcon />
-      <Menu className='header-menu' mode="horizontal" items={items} />
+      <Menu className='header-menu' mode="horizontal" items={items} />   
     </Header>
   );
 };
