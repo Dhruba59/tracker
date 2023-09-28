1cb30f5cdc6cb57611e77155bc80098e7e549c05
Dhruba Jyoti Sarker <dhruba59.sust@gmail.com>
2023-09-28 15:28:00 +0600

    add activity img and date

--- src/features/tracker/activity-bar/activity/activity.tsx
+++ src/features/tracker/activity-bar/activity/activity.tsx
@@ -1,20 +1,24 @@
 import { Avatar, Typography } from 'antd';
 import './activity.css';
 import { formatTime } from '@helpers/global-helpers';
+import UserAvatar from '@components/common/user-avatar';
 
 const { Text } = Typography;
 
 const Activity = ({ activity }: any) => {
   const message = activity?.message?.split('called');
-  
+  const dateEvent = new Date(activity?.created_at);
+  const options: any = { year: 'numeric', month: 'long', day: 'numeric' };
+  const date = dateEvent.toLocaleDateString(undefined, options);
+
   return (
     <div className='activity-container'>
       <div className='activity-dp'>
-        <Avatar size='large' />
+        <UserAvatar title={activity?.created_by?.name} src={activity?.created_by?.profile_image_url} />
       </div>
       <div className='activity-col-2'>
         <Text className='activity-title'>{message[0]} {message.length > 1 ? 'called': ''}<span> {message[1]}.</span></Text>
-        <Text className='activity-time-user'>{formatTime(activity.created_at)} by <span>{activity.created_by.name}</span></Text>
+        <Text className='activity-time-user'>{date} at {formatTime(activity.created_at)} by <span>{activity.created_by.name}</span></Text>
       </div>
     </div>
   );
